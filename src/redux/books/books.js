f38f5743b0d47a911f22875dc7e329ffe7592096
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2022-05-02 17:30:57 +0300

    Reducers and actions for books and categories added

---
+++ src/redux/books/books.js
@@ -0,0 +1,29 @@
+const ADDBOOK = bookstore/books/ADDBOOK;
+const REMOVEBOOK = bookstore/books/REMOVEBOOK;
+
+export default reducer = (state={},action) => {
+switch(action.type)
+{
+    case ADDBOOK:
+        return{ state = {
+            title: 'ABC book',
+            author: 'Demse',
+        }}
+    case REMOVEBOOK:
+    return state={};
+    default:
+        return state;
+
+}
+}
+
+export const addBook =()=> {
+    return{
+        type:ADDBOOK,
+    }
+}
+export const removeBook = ()=>{
+    return{
+        type: REMOVEBOOK
+    }
+}
